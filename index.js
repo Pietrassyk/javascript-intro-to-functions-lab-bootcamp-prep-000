0f70b356177e106fa814383c4309e5c7fbf27113
Tino Pietraßyk <pietrassyk@googlemail.com>
2019-02-21 17:48:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,7 @@ function sayHiToGrandma(string){
   if (string === "I love you, Grandma."){
     return("I love you, too.")
   }
+  if(string === string.toLowerCase()){
+    return("I can't hear you!")
+  }
 }
